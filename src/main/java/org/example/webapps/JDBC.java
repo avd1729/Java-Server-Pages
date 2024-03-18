048d02130de31011d0a74cd7447dfa670d725167
avd <aravind19112003@gmail.com>
2024-03-18 21:01:21 +0530

    15.MySQL conn

--- src/main/java/org/example/webapps/JDBC.java
+++ src/main/java/org/example/webapps/JDBC.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+// 15th question
 public class JDBC {
     public static void main(String[] args) {
         Connection conn = null;
